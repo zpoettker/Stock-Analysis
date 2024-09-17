766a819dd59128f56ecf7d763ee97355682ab8e7
zpoettker <poettkerzach@gmail.com>
2024-09-17 21:43:41 +0000

    embeded stock choice

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
+function getQueryParam(param) {
+    let urlSearchParams = new URLSearchParams(window.location.search);
+    return urlSearchParams.get(param);
+}
+
+function readSymbolFromQueryString() {
+    return getQueryParam('tvwidgetsymbol');
+}
+
+function cloneTemplateInto(templateId, targetId, rewrites) {
+    const tmpl = document.querySelector(`#${templateId}`);
+    if(!tmpl) return;
+    const target = document.querySelector(`#${targetId}`);
+    if(!target) return;
+    target.innerText = '';
+    const clone = tmpl.content.cloneNode(true);
+    if (rewrites) {
+        const script = clone.querySelector('script');
+        script.textContent = rewrites(script.textContent);
+    }
+    target.appendChild(clone);
+}
+
+const symbol = readSymbolFromQueryString() ||'NASDAQ:TSLA';
+
+function setSymbol(scriptContent){
+    return scriptContent.replace(/"symbol": "([^"]*)"/g, () => {
+        return `"symbol": "${symbol}"`;
+    });
+}
+
+cloneTemplateInto('symbol-info-template', 'symbol-info', setSymbol);
+cloneTemplateInto('advanced-chart-template', 'advanced-chart');
+cloneTemplateInto('company-profile-template', 'company-profile', setSymbol);
+cloneTemplateInto('fundamental-data-template', 'fundamental-data', setSymbol);
+cloneTemplateInto('technical-analysis-template', 'technical-analysis', setSymbol);
+cloneTemplateInto('top-stories-template', 'top-stories', setSymbol);
+
+if(symbol){
+    document.title = `stock Details - ${symbol}`;
+}
 
