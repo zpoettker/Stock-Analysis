77b6302209404c7323bf9747b65520ea76574d27
zpoettker <poettkerzach@gmail.com>
2024-10-13 04:14:36 +0000

    finished rich text editor

--- index.js
+++ index.js
@@ -40,3 +40,18 @@ if(symbol){
     document.title = `stock Details - ${symbol}`;
 }
 
+const elements = document.querySelectorAll('.btn');
+
+// Event
+elements.forEach(element => {
+    element.addEventListener('click', () => {
+        let command = element.dataset['element'];
+
+        if(command == 'createLink' || command == 'insertImage') {
+            let url = prompt('Enter the link here:', 'https://');
+            document.execCommand(command, false, url);
+        } else {
+            document.execCommand(command, false, null);
+        }
+    });
+});
